83d0cf8e43c76981003ff5b932adf51994bdc8c6
philipkogel <philipkogel@yahoo.pl>
2023-03-02 21:27:27 +0100

    feat(NewsFeed): dynamic set intrests and language, example render of email body

--- app/main.py
+++ app/main.py
@@ -3,5 +3,10 @@ Main app file
 """
 from models import NewsFeed
 
-nf = NewsFeed(data="books")
-nf.get()
+nf = NewsFeed(interest="nasa")
+news = nf.get()
+example_email_body = ''
+for n in news:
+    example_email_body += n['title'] + '\n' + n['url'] + '\n\n'
+
+print(example_email_body)
