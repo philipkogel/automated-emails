46534eb7e3ead3f54a7c9724b23beb7bf46c4771
philipkogel <philipkogel@yahoo.pl>
2023-03-05 21:46:04 +0100

    feat(MailGun): emails sent via mailgun with news feed raw data

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 """
 Main app file
 """
-from models import NewsFeed
+from models import NewsFeed, Email
 
 nf = NewsFeed(interest="nasa")
 news = nf.get()
@@ -9,4 +9,5 @@ example_email_body = ''
 for n in news:
     example_email_body += n['title'] + '\n' + n['url'] + '\n\n'
 
-print(example_email_body)
+email = Email(to='tafape7969@wwgoc.com', body=example_email_body)
+req = email.send_simple_message()
--- app/models/Email.py
+++ app/models/Email.py
@@ -1,5 +1,22 @@
+import requests
+from dotenv import dotenv_values
 class Email:
     """
     Represents a email model
     """
-    pass
+    domain_name = dotenv_values(".env")["MAILGUN_DOMAIN"]
+    api_key = dotenv_values(".env")["MAILGUN_API_KEY"]
+    def __init__(self, to: str, body: str):
+        self.to = to
+        self.body = body
+    def send_simple_message(self):
+        """
+        Implementation for sending out emails via https://www.mailgun.com/
+        """
+        return requests.post(
+            f"https://api.mailgun.net/v3/{self.domain_name}/messages",
+            auth=("api", f'{self.api_key}'),
+            data={"from": f"Excited User <mailgun@{self.domain_name}>",
+                  "to": [self.to],
+                  "subject": "Hello",
+                  "text": self.body})
